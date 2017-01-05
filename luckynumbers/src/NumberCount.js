4d854e097d50c4133592e2c96d26c27c836e9b2a
m <martin.tierney86@hotmail.co.uk>
2017-01-05 21:26:18 +0000

    initial

---
+++ luckynumbers/src/NumberCount.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NumberCount extends React.Component {
+  render() {
+    return (
+        <div>
+        <h1>How many numbers do you need?</h1>
+        
+        </div>
+    );
+  }
+}
+
+export default NumberCount;
---
+++ luckynumbers/src/NumberForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import NumberCount from './NumberCount';
+
+class NumberForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: '',
+            min: '',
+            max: ''
+        };
+
+        this.handleCountChange = this.handleCountChange.bind(this);
+        this.handleMinChange = this.handleMinChange.bind(this);
+        this.handleMaxChange = this.handleMaxChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleCountChange(event) {
+        this.setState({ count: event.target.value });
+    }
+
+    handleMinChange(event) {
+        this.setState({ min: event.target.value });
+    }
+
+    handleMaxChange(event) {
+        this.setState({ max: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.count + ',' + this.state.min + ',' + this.state.max);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        How many numbers do you want?
+          <input type="text" value={this.state.count} onChange={this.handleCountChange} />
+                    </label>
+                    <label>
+                        What is your minimum number?
+          <input type="text" value={this.state.min} onChange={this.handleMinChange} />
+                    </label>
+                    <label>
+                        What is your maximum number?
+          <input type="text" value={this.state.max} onChange={this.handleMaxChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <NumberCount />
+            </div>
+        );
+    }
+}
+
+export default NumberForm;
