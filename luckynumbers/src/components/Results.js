28a91764a691a3e7725394e6bd7bf51abbaf58de
m <martin.tierney86@hotmail.co.uk>
2017-01-28 18:37:46 +0000

    results heading now conditional

--- luckynumbers/src/components/Results.js
+++ luckynumbers/src/components/Results.js
@@ -2,9 +2,16 @@ import React, { PropTypes } from 'react'
 import Result from './Result'
 import '../css/Results.css';
 
+function showHeading(check) {
+    if (check.length > 0) {
+        return <h2 className="results-title">Your Lucky Numbers!</h2>
+    }
+}
+
 const Results = ({ Results }) => (
     <div>
-        <h2 className="results-title">Your Lucky Numbers!</h2>
+        {showHeading(Results)}
+       
         {Results.map((item, index) =>
             // <h3 key={index}>{item}</h3>
             <Result key={index} Result={item}/>
